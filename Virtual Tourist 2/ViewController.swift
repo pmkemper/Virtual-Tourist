6614af0a41ed316196e7c491e114bb6cee47b38b
Philip Kemper <pmkemper@gmail.com>
2020-01-24 13:49:01 -0500

    Initial Commit

---
+++ Virtual Tourist 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist 2
+//
+//  Created by Philip Kemper on 24/01/2020.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
